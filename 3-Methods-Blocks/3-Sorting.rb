2f14a0edf38a6dda34550176f417fcf29a7cb38d
dbaggie <justinharris3j@gmail.com>
2022-10-09 23:06:47 -0500

    Added 3-Sorting

---
+++ 3-Methods-Blocks/3-Sorting.rb
@@ -0,0 +1,14 @@
+# This lesson had some random sorting methods thrown in
+# Here are some examples
+
+
+# Random Method Sort
+my_array = [5, 15, 3, 100, 42, 20, 13]
+puts my_array.sort!
+
+books = ["Charlie and the Chocolate Factory", "War and Peace", "Utopia", "A Brief History of Time", "A Wrinkle in Time"]
+# To sort our books in ascending order, in-place
+books.sort! { |firstBook, secondBook| firstBook <=> secondBook }
+
+# Sort your books in descending order, in-place below
+books.sort! { |firstBook, secondBook| secondBook <=> firstBook }
