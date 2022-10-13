2a63ff02d092220d4d7545d0f41e8c9ddb9c743f
dbaggie <justinharris3j@gmail.com>
2022-10-12 21:06:35 -0500

    Updated hash benchmark

--- 4-Hashes-Symbols/2-Hash-Benchmark.rb
+++ 4-Hashes-Symbols/2-Hash-Benchmark.rb
@@ -4,11 +4,11 @@ string_AZ = Hash[("a".."z").to_a.zip((1..26).to_a)]
 symbol_AZ = Hash[(:a..:z).to_a.zip((1..26).to_a)]
 
 string_time = Benchmark.realtime do
-  100_000.times { string_AZ["r"] }
+  500_000.times { string_AZ["r"] }
 end
 
 symbol_time = Benchmark.realtime do
-  100_000.times { symbol_AZ[:r] }
+  500_000.times { symbol_AZ[:r] }
 end
 
 puts "String time: #{string_time} seconds."
