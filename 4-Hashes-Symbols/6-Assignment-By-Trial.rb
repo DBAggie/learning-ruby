a447013cd3f9ea2452e726a5bb654afdde43ac2d
dbaggie <justinharris3j@gmail.com>
2022-10-13 20:54:59 -0500

    Add Assignments

---
+++ 4-Hashes-Symbols/6-Assignment-By-Trial.rb
@@ -0,0 +1,16 @@
+# Assignments can be made only if the variable is nil
+
+# ||= Doesn't work here
+print "Enter a number: "
+num = gets.chomp
+puts "Your number is currently: #{num}"
+num ||= num + 1
+puts "Your number is currently: #{num}"
+
+# ||= Works here
+num = nil
+num ||=10
+puts "Your number is currently: #{num}"
+
+# Why would you use this? I guess as a barrier for reassignment of a variable
+# This is cool but I can't imagine this gets used often
