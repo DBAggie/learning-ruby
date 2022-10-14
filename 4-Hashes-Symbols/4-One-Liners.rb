8b5b70bd1a508d3cecd7c6d521358d9380ebcb87
dbaggie <justinharris3j@gmail.com>
2022-10-13 20:46:27 -0500

    Added one liners

---
+++ 4-Hashes-Symbols/4-One-Liners.rb
@@ -0,0 +1,9 @@
+# One Liner Mania
+
+variable = true
+
+puts "This is a one liner!" if variable == true
+
+puts "Here I am again" unless false # <== toggle this to true to see the output
+puts 1 < 3 ? "and again" : "How do we get this to print?" # <== can you figure it out?
+
