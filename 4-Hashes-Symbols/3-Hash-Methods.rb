a0f179bfe59cbc0b01349a99a1b88d17d626afb5
dbaggie <justinharris3j@gmail.com>
2022-10-13 20:15:11 -0500

    Updated output of Hash Methods file

--- 4-Hashes-Symbols/3-Hash-Methods.rb
+++ 4-Hashes-Symbols/3-Hash-Methods.rb
@@ -10,6 +10,13 @@ superhero_genders = {
 
 # Select something specific
 female_superheros = superhero_genders.select { |key, value| value == "Female" }
+puts "Iterate by each record"
 female_superheros.each { |key, value| puts key.to_s.capitalize} # <== This can be altered to clean up the output
 
+puts ""
 # .each_key or .each_value
+puts "Iterate by Key"
+female_superheros.each_key { |key| puts key.to_s.sub("_", " ").capitalize}
+puts ""
+puts "Iterate by Value"
+female_superheros.each_value { |value| puts value.capitalize}
